7467c1b963032b98f786aad4a82b8faeea2f23aa
Tiago Avelino <tavelino@redhat.com>
2024-04-04 09:59:06 -0300

    ajuste project

--- src/main/java/com/example/simplerest/demo/PostKafka.java
+++ src/main/java/com/example/simplerest/demo/PostKafka.java
@@ -1,3 +1,5 @@
+package com.example.simplerest.demo;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
