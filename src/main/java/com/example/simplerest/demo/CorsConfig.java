f985714dec7c1644025d78c2c8c5cd8e6c463934
Tiago Avelino <tavelino@redhat.com>
2024-04-03 22:36:33 -0300

    ajuste rota

--- src/main/java/com/example/simplerest/demo/CorsConfig.java
+++ src/main/java/com/example/simplerest/demo/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000")
+            .allowedOrigins("https://spring-rest-simple-rest.apps.u0eqq9nm.eastus.aroapp.io")
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("*");
     }
