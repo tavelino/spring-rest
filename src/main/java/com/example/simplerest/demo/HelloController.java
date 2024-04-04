65995adfdf031189e1fc168f8909a6b0e4013616
Tiago Avelino <tavelino@redhat.com>
2024-04-04 10:34:21 -0300

    ajuste

--- src/main/java/com/example/simplerest/demo/HelloController.java
+++ src/main/java/com/example/simplerest/demo/HelloController.java
@@ -2,12 +2,27 @@ package com.example.simplerest.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @RestController
 public class HelloController {
+
+    @Autowired
+    private KafkaProducerService kafkaProducerService;
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello bbassets!";
     }
+    
+    @PostMapping("/hello")
+    public String hello(@RequestBody String message) {
+        kafkaProducerService.sendMessage(message); // Call the sendMessage method
+        return "Message sent to Kafka topic: " + message;
+    }
+
 }
